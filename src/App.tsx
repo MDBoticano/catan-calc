989f417baa5c4dd653828a7f05ff5e9cf6013715
MDBoticano <MDBoticano@users.noreply.github.com>
2020-02-27 18:41:09 -0800

    Add form to set territory values to calculate

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 import Catan from './components/Catan';
 
-const myCatan = new Catan();
-const adjacent345 = myCatan.calculateChances([3, 4, 5]);
-
 const App = () => {
+  const [chances, setChances] = useState([0, 0, 0]);
+  const [totalChance, setTotalChance] = useState(0);
+  const [myCatan] = useState(new Catan());
+
+  const modifyChances = (index: number, value: number) => {
+    const newChances: number[] = chances;
+    newChances[index] = value;
+    setChances(newChances);
+  }
+
+  const calculate = (event: React.SyntheticEvent) => {
+    event.preventDefault();
+    const calculation = myCatan.calculateChances(chances);
+    console.log(calculation);
+    setTotalChance(calculation);
+  }
+
   return (
     <div className="App">
-      {adjacent345}
+      <input type="number" min="2" max="12" onChange={(e) => modifyChances(0, Number(e.target.value))}/>
+      <input type="number" min="2" max="12" onChange={(e) => modifyChances(1, Number(e.target.value))}/>
+      <input type="number" min="2" max="12" onChange={(e) => modifyChances(2, Number(e.target.value))}/>
+      <button type="submit" onClick={(e) => calculate(e) }>Calculate</button>
+
+      <div>
+        Result: {totalChance} / 36
+      </div>
+      
     </div>
   );
 }
